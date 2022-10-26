da2aa5bfacdfd4acfd858e13cc4089091f9c5351
irekc <irekc86@o2.pl>
2022-10-26 07:24:54 +0200

    feat: Add products

--- components/Products.tsx
+++ components/Products.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import { Rating } from "./Ratings";
 
@@ -18,7 +19,14 @@ interface ProductDetailsProps {
 export const ProductDetails = ({ data }: ProductDetailsProps) => {
   return (
     <>
-      <img src={data.thumbnailUrl} alt={data.thumbnailAlt} />
+      <div className="bg-white p-4">
+      <Image src={data.thumbnailUrl} alt={data.thumbnailAlt} 
+      layout="responsive"
+      width={16}
+      height={9}
+      objectFit="contain"
+      />
+    </div>
       <h2 className="p-4 text-3xl font-bold">{data.title}</h2>
       <p className="p-4">{data.description}</p>
       <Rating rating={data.rating}/>
@@ -37,7 +45,14 @@ interface ProductsListItemProps {
 export const ProductListItem = ({ data }: ProductsListItemProps) => {
   return (
     <>
-      <img src={data.thumbnailUrl} alt={data.thumbnailAlt} />
+    <div className="bg-white p-4">
+      <Image src={data.thumbnailUrl} alt={data.thumbnailAlt} 
+      layout="responsive"
+      width={16}
+      height={9}
+      objectFit="contain"
+      />
+    </div>
       <Link href={`/products/${data.id}`}>
         <a>
           <h2 className="p-4 text-3xl font-bold">{data.title}</h2>
