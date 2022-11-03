26464afc9a09661eb66b9ead48fee1311cdbd098
irekc <irekc86@o2.pl>
2022-11-03 06:35:21 +0100

    remove products csr

--- next.config.js
+++ next.config.js
@@ -4,11 +4,7 @@ const nextConfig = {
   swcMinify: true,
   trailingSlash: true,
   images: {
-<<<<<<< HEAD
     domains: ["naszsklep-api.vercel.app", "picsum.photos"],
-=======
-    domains: ["fakestoreapi.com"],
->>>>>>> 2d5eb02e6d7ad4806852ccfeed98972862354b78
     formats: ['image/avif', 'image/webp'],
   }
 }
